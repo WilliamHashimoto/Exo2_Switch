ed159e61040cd72fccf69b5eb5873aa6a94eff92
WilliamHashimoto <79861381+WilliamHashimoto@users.noreply.github.com>
2021-04-20 21:25:36 +0200

    fini

---
+++ public/js/main.js
@@ -0,0 +1,16 @@
+let meteo = prompt("Entrez le type de météo")
+
+switch (meteo)) {
+    case "nuageux":
+        console.log("Il fait semi moche");
+        break;
+
+    case "pluvieux":
+        console.log("Il fait moche");
+        break;
+    case "ensoleillé":
+        console.log("Il fait beau");
+        break;
+    default:
+        break;
+}
